1875a0c42fa6e4f5add20cdc529a354d5888006b
Artur Gabrielyan <artur.gabrielyan@inconceptlabs.com>
2025-04-15 11:47:39 +0400

    Batch Insert

--- src/main/java/org/example/springdatademo/persistence/repository/EmployeeRepository.java
+++ src/main/java/org/example/springdatademo/persistence/repository/EmployeeRepository.java
@@ -2,10 +2,19 @@ package org.example.springdatademo.persistence.repository;
 
 import org.example.springdatademo.persistence.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Employee findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    @Query("SELECT e.email FROM Employee e")
+    Set<String> findAllEmails();
 }
